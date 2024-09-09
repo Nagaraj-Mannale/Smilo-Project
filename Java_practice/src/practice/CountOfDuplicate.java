62baba9f28dc75d2f30aeb2209de47af4691b0d1
nagaraj <nagaraj@active.agency>
2024-09-09 18:29:19 +0530

    PRacticed code of an Arrays Concept

---
+++ Java_practice/src/practice/CountOfDuplicate.java
@@ -0,0 +1,23 @@
+package practice;
+
+
+import java.util.Scanner;
+
+public class CountOfDuplicate {
+public static void main(String[] args) {
+   int[]a= {10,20,10,30,40,50};
+  
+   int count=0;
+   Scanner scanner=new Scanner(System.in);
+  System.out.println("Give the input");
+   int b=scanner.nextInt();
+   for(int i=0;i<a.length;i++)
+   {
+	   if(a[i]==b)
+	   {
+		   count++;
+	   }
+   }
+   System.out.println(b+" is "+count+ " times repeated in the given array");
+}
+}
---
+++ Java_practice/src/practice/FindingAnGivenElementInArrays.java
@@ -0,0 +1,22 @@
+package practice;
+import java.util.*;
+public class FindingAnGivenElementInArrays {
+public static void main(String[] args) {
+	int[]a= {10,20,30,40,50};
+	boolean value=false;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Give the Input for Find");
+	int b=sc.nextInt();
+	for(int i=0;i<a.length;i++)
+	{
+		if(a[i]==b)		
+		{
+			System.out.println("Given input is present in the Array");
+			value=true;break;
+		}	}
+	if(value==false)
+	{
+		System.out.println("Given input is not present in the Array");
+	}
+}
+}
